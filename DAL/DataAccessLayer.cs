3e7a726f7bcada1001bfbe4ec8c32ea2700fe48d
Cankugu <cankugu@gmail.com>
2019-01-17 14:20:44 +0300

    ürün eklendi

--- DAL/DataAccessLayer.cs
+++ DAL/DataAccessLayer.cs
@@ -22,25 +22,31 @@ namespace DAL
         
         public int UrunEkle(Urunler u)
         {
-            string sqlsorgu = ""
+            
             int SorguKontrol = 0;
+            
             string adress = "Data Source=.;Initial Catalog=Heddiye;Integrated Security=True";
              con = new SqlConnection(adress);
 
-            var query = con.Query<Urunler>("SP_UrunEkle",
-                new
-                {
-                    UrunAdii = u.UrunAdi,
-                    Cinsiyet = u.CinsiyetAdi,
-                    HediyeAmacii = u.HediyeAmaci,
-                    YakinlikDerecesii = u.YakinlikDerecesi,
-                    YasAraligii = u.YasAraligi,
-                    Burcadii = u.BurcAdi
-                }, commandType: CommandType.StoredProcedure);
-            if (query.Count() > 0)
+            
+
+            SorguKontrol = con.Execute("SP_UrunEkle", new
+            {
+                UrunAdii = u.UrunAdi,
+                Cinsiyet = u.CinsiyetAdi,
+                HediyeAmacii = u.HediyeAmaci,
+                YakinlikDerecesii = u.YakinlikDerecesi,
+                YasAraligii = u.YasAraligi,
+                Burcadii = u.BurcAdi
+            }, commandType: CommandType.StoredProcedure);
+            if (SorguKontrol > 0)
             {
                 SorguKontrol = 1;
             }
+            //if ( ((System.Collections.Generic.List<DAL.Tablo_Sınıfları.Urunler>)query).Count>=0)
+            //{
+            //    SorguKontrol = 1;
+            //}
 
             return SorguKontrol;
 
